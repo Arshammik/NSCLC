3690b08eddc2f0cb38fae993497214af48aaa840
Arshammik <79arsham@gmail.com>
2024-02-12 01:14:11 -0500

    starting assigning cell types

--- r_scripts/2.sacling_and_finding_markers_from_integrated_object.r
+++ r_scripts/2.sacling_and_finding_markers_from_integrated_object.r
@@ -27,5 +27,29 @@ pbmc <- FindClusters(object = pbmc, resolution = 0.5)
 pbmc <- FindClusters(object = pbmc, resolution = 0.3)
 
 pbmc.markers <- FindAllMarkers(pbmc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+saveRDS(pbmc.markers, "/home/arsham79/nsclc/results/pbmc.markers.rds")
+
+
+sorted_p_values <- sort(pbmc.markers$p_val)
+expected_quantiles <- -log10((1:length(sorted_p_values)) / length(sorted_p_values))
+qqplot(sorted_p_values, expected_quantiles,
+       xlab = 'Expected -log10(p-value)',
+       ylab = 'Observed -log10(p-value)',
+       main = 'QQ Plot',
+       col = 'black',
+       pch = 20) 
+abline(coef = c(0,1))
+
+clusters <- data.table(cluster_number = c(4, ),
+                       cluster_cell = c("B cells", ))
+## B cells
+FeaturePlot(object = pbmc, features = c("MS4A1","CD19","CD79A"), raster = FALSE, label = TRUE, label.size = 3)
+
+
+## T cells
+FeaturePlot(object = pbmc, features = c("TRBC2","CD3D","CD3G"), raster = FALSE, label = TRUE, label.size = 3)
+
+## NK cells
+FeaturePlot(object = pbmc, features = c(), raster = FALSE, label = TRUE, label.size = 3)
 
 
